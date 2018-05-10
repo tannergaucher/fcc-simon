31ce7fdb46f306849a1e86d9d04b98ff6e9cdf90
tanner gaucher <tannermichaelgaucher@gmail.com>
2018-05-10 13:09:49 +0800

    working simon and check

--- js/index.js
+++ js/index.js
@@ -9,7 +9,10 @@ let sound4 = document.getElementById('sound4');
 let onSwitch = document.getElementById('checkbox');
 let controls = document.getElementById('controls');
 let simonBody = document.getElementById('simon');
-
+let start = document.getElementById('start');
+let counter = document.getElementById('counter');
+let strictLight = document.getElementById('strict-light');
+let buttons = document.querySelectorAll('div.button');
 
 let colors = ['red', 'green', 'blue', 'yellow'];
 let count = 1;
@@ -19,15 +22,19 @@ function getRandomColor() {
   return colors[Math.floor(Math.random() * colors.length)];
 }
 
+// 1. simon chooses random color, clicks 
 function simon() {
+  console.log('simon call');
   let rand = getRandomColor();
   que.push(rand);
+  document.getElementById(rand).click();
 }
 
 function player(button) {
   if (button === que[count - 1]) {
     console.log("match");
     count++;
+    //next turn
     simon();
 
   } else {
@@ -37,18 +44,40 @@ function player(button) {
   }
 }
 
+// 3. compare arrays for match
+function check() {
+
+}
+
 function switchOn() {
   if (onSwitch.checked === true) {
     controls.classList.add('on');
     controls.classList.remove('off');
-
+    powerButtons();
   }
   if (onSwitch.checked === false) {
     controls.classList.add('off');
     controls.classList.remove('on');
+    counter.classList.add('off');
+    powerButtons();
   }
 }
 switchOn();
+
+function powerButtons() {
+  buttons.forEach(function(button) {
+    button.classList.toggle('buttons-off');
+  })
+}
+
+start.addEventListener('click', function() {
+  counter.classList.toggle('off');
+  //simon();
+})
+
+strictLight.addEventListener('click', function() {
+  strictLight.classList.toggle('strict-light-on');
+})
 
 onSwitch.addEventListener('click', switchOn);
 
